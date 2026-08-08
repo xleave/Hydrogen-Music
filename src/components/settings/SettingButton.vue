96e649b96f44055b8aff5a44373707b01ba9d6c6
Y4n <63758980+xleave@users.noreply.github.com>
2026-08-08 15:13:37 +0100

    refactor: encode settings button variants in shared control

--- src/components/settings/SettingButton.vue
+++ src/components/settings/SettingButton.vue
@@ -5,8 +5,6 @@ const props = defineProps({
   width: { type: [Number, String], default: null },
   padding: { type: String, default: '6px' },
   fontSize: { type: Number, default: 14 },
-  hoverOpacity: { type: Number, default: 1 },
-  hoverBackground: { type: String, default: 'rgba(255, 255, 255, .35)' },
 })
 defineEmits(['click'])
 
@@ -14,8 +12,6 @@ const style = computed(() => ({
   width: props.width == null ? undefined : (typeof props.width === 'number' ? `${props.width}px` : props.width),
   padding: props.padding,
   fontSize: `${props.fontSize}px`,
-  '--setting-button-hover-opacity': String(props.hoverOpacity),
-  '--setting-button-hover-background': props.hoverBackground,
 }))
 </script>
 
@@ -39,9 +35,15 @@ const style = computed(() => ({
   cursor: pointer;
 
   &:hover {
-    opacity: var(--setting-button-hover-opacity);
-    background-color: var(--setting-button-hover-background);
     box-shadow: inset 0 0 0 1px black;
+  }
+
+  &.add-option:hover {
+    opacity: .8;
+  }
+
+  &.default-shortcuts:hover {
+    background-color: rgba(255, 255, 255, .48);
   }
 }
 </style>
