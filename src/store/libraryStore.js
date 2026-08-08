66cce350b1768e97fcc64e8a2c644e7099bfba74
Y4n <63758980+xleave@users.noreply.github.com>
2026-08-08 13:08:40 +0100

    perf: separate folder expansion from library data

--- src/store/libraryStore.js
+++ src/store/libraryStore.js
@@ -5,5 +5,21 @@ export const useLibraryStore = defineStore('libraryStore', {
     listType1: 3,
     listType2: 0,
     libraryChangeAnimation: false,
+    expandedFolderIds: {},
   }),
+  actions: {
+    folderExpanded(id) {
+      return Boolean(id && this.expandedFolderIds[id])
+    },
+    toggleFolder(id) {
+      if (!id) return
+      const next = { ...this.expandedFolderIds }
+      if (next[id]) delete next[id]
+      else next[id] = true
+      this.expandedFolderIds = next
+    },
+    clearExpandedFolders() {
+      this.expandedFolderIds = {}
+    },
+  },
 })
